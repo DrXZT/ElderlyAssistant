f7a0d575872afa30faee4d0e1c160b9a4e6a747d
xzt <710007817@qq.com>
2019-07-06 15:19:06 +0800

    first

---
+++ app/src/main/java/com/asciimovie/drxzt/elderlyassistant/Activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.asciimovie.drxzt.elderlyassistant.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.asciimovie.drxzt.elderlyassistant.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
