48339773de12322f8fc6a9aedfa569f65885373a
Thereforu <244097155@qq.com>
2019-08-16 14:18:49 +0800

    11

---
+++ app/src/main/java/com/asciimovie/drxzt/elderlyassistant/Activity/user/Fragment_food_1.java
@@ -0,0 +1,22 @@
+package com.asciimovie.drxzt.elderlyassistant.Activity.user;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.asciimovie.drxzt.elderlyassistant.R;
+
+public class Fragment_food_1 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_food_1, container, false);
+        return view;
+    }
+}
