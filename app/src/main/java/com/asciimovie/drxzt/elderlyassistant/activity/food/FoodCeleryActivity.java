d79945804d8116c19f9d34d1f1ac068e20fb32c2
myyouthismine <1225463516@qq.com>
2019-09-01 16:29:19 +0800

    emmmmmm dbq

---
+++ app/src/main/java/com/asciimovie/drxzt/elderlyassistant/activity/food/FoodCeleryActivity.java
@@ -0,0 +1,15 @@
+package com.asciimovie.drxzt.elderlyassistant.activity.food;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.asciimovie.drxzt.elderlyassistant.R;
+
+public class FoodCeleryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_celery);
+    }
+}
